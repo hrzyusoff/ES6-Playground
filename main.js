9708b07501f9fd29a1e683cabc025a6233a44f0a
hrzyusoff <hrzyusoff@gmail.com>
2018-07-13 02:26:27 +0800

    update main.js

--- main.js
+++ main.js
@@ -12,6 +12,30 @@ c = '999'
 // a === c // true
 // a === c // false
 
+// OBJECT
+let books = new Object(); //let user = {} alt ways
+// key: value
+books = {
+    author: "Hariz Yusoff",
+    title: "Intro to Programming" 
+}
+
+// Adding key
+books.versioning = 99
+books.isBorrowed = true
+
+let newkey = "page number"
+books[newkey] = 200
+// Delete Key
+delete books.isBorrowed
+
+console.log(typeof books)
+console.log(books)
+
+
+let userInput = prompt("What you want know about this book ?")
+console.log(books[userInput])
+// END OF OBJECT
 
 // "Object in organization is "+organization, "Items in organization at D is "+organization.d, "Compare value (===) 999 and '999' is "+a === c, "Splitting a in aaa999sd&dsjksaaasd : "+b.split('a')
 console.log(organization, organization.d, a === c, b.split('a')) //
